b195c8b8ccc8bbe8a43d74d92a8fcd42d1acf579
Kstxiz candido <srfelipe.my@gmail.com>
2025-02-17 17:48:42 +0000

    CategoriaController terminada

--- app/src/main/java/application/model/Categoria.java
+++ app/src/main/java/application/model/Categoria.java
@@ -23,4 +23,33 @@ public class Categoria { // Declara a classe Categoria, que representa uma categ
     
     @Column(unique = true, nullable = false) // Define que a coluna "nome" deve ser única e não pode ser nula
     private String nome; // Declara o atributo "nome" do tipo String para armazenar o nome da categoria
+
+    @OneToMany(mappedBy = "categoria")
+    private Set<Jogo> Jogos = new HashSet<>();
+
+    public long getID(){
+        return id;
+    }
+
+    public void setId(long id) {
+    this.id = id;
+    }
+
+    public String getNome() {
+    return nome;
+    }
+
+    public void setNome (String nome) {
+    this.nome = nome;
+    }
+    
+    public Set<Jogo> getJogos() {
+    return jogos;
+    }
+
+    public void setJogos (Set<Jogo> jogos) {
+    this.jogos = jogos;
+    }
+
 }
+
